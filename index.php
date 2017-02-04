7482921008878e9bc4f6c45f581988d24893866e
Liz Bright <elizabeth.anne.bright@gmail.com>
2017-02-04 16:56:43 -0500

    added content to php file; added style sheet

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+
+<head>
+	<meta charset="utf-8">
+
+	<title>Liz Bright Webpage</title>
+	<!--my stylesheet -->
+	<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+
+<body>
+
+	<div class="wrapper">
+
+		<header>
+			<h1>Liz Bright</h1>
+		</header>
+
+		<div class="container">
+			<img src="images/vines.jpg" id="vines" alt="vines" />
+
+			<h2>About Me</h2>
+			<div id="intro">
+				<div class="text">
+					<p>We shall not cease from all exploration</p>
+				</div>
+			</div>
+
+			<img src="image/vines.jpg" id="vines" alt="vines" />
+
+			<h2>Random Quote</h2>
+			<div id="quote">
+				<div class="text">
+					<blockquote>We shall not cease from all exploration<br/> And the end of all of all our exploring<br/> Will be to arrive where we started<br/> And know the place for the first time. -T.S. Eliot </blockquote>
+				</div>
+			</div>
+
+		</div>
+
+		<footer>
+			<div id="footer">
+				<p>February 2017</p>
+			</div>
+		</footer>
+
+	</div>
+	<!-- close wrapper -->
+
+	</html>
