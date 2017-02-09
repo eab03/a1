2bdfb9d8d66537a1fb66bb8e6b358fedb3657bd9
Liz Bright <elizabeth.anne.bright@gmail.com>
2017-02-09 16:09:17 -0500

    added elementary style for mobile devices and adjusted padding around elements

--- index.php
+++ index.php
@@ -6,9 +6,11 @@
 
 <head>
 	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<title>Liz Bright Introduction</title>
-	<link rel="stylesheet" type="text/css" href="style.css" />
+
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 
 <body>
